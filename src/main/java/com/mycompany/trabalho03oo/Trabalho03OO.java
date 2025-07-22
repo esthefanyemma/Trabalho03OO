35bb5341778c00f39ee2529353faf0f45f4c4f8a
AureaStar <aurea.starlight@gmail.com>
2025-07-21 22:01:44 -0300

    Configuração Inicial do Projeto com Maven

---
+++ src/main/java/com/mycompany/trabalho03oo/Trabalho03OO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalho03oo;
+
+/**
+ *
+ * @author 84398
+ */
+public class Trabalho03OO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
